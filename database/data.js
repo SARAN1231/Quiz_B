64ae2d3bce220640661c557c13e147eabc83e5dc
SARAN1231 <saranprakash3112@gmail.com>
2023-12-14 00:33:46 +0530

    1 commit

---
+++ database/data.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    id: 1,
+    question:
+      "What is the typical method by which ransomware gains access to a system?",
+    options: ["Phishing emails", "Software updates", "Social media posts"],
+  },
+  {
+    id: 2,
+    question: "What is the purpose of the encryption used by ransomware?",
+    options: [
+      "To steal sensitive information",
+      "To make files inaccessible",
+      "To enhance system performance",
+    ],
+  },
+  {
+    id: 3,
+    question: "How do attackers often demand payment in ransomware attacks?",
+    options: [
+      " Credit card transactions",
+      "Cryptocurrency transactions",
+      "Bank transfers",
+    ],
+  },
+  {
+    id: 4,
+    question:
+      "Which of the following is a common target of ransomware attacks?",
+    options: [
+      "Physical infrastructure",
+      "Educational institutions",
+      "Healthcare organizations",
+    ],
+  },
+  {
+    id: 5,
+    question: "What is the best defense against ransomware attacks?",
+    options: [
+      "Ignoring software updates",
+      "Disabling antivirus software",
+      "Regularly backing up data",
+    ],
+  },
+];
+
+export const answers = [0,1,1,2,2];
